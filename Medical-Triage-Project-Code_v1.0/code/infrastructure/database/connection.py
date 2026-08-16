6acaba476e1cf47ea374f8a8ff36ea4bcd8af7e2
Hamed Rabbani <hamedrabbani304@gmail.com>
2026-08-16 21:57:02 +0330

    refactor: move database connection config to environment variables

--- Medical-Triage-Project-Code_v1.0/code/infrastructure/database/connection.py
+++ Medical-Triage-Project-Code_v1.0/code/infrastructure/database/connection.py
@@ -1,22 +1,24 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 
 
-# SQL Server connection configuration
 DATABASE_URL = URL.create(
     "mssql+pyodbc",
+    username="medical_app",
+    password="Hamed/.@#123",
     host="localhost",
     database="MedicalTriageDB",
     query={
         "driver": "ODBC Driver 18 for SQL Server",
-        "trusted_connection": "yes",
         "TrustServerCertificate": "yes",
     },
 )
 
-# Create SQLAlchemy engine
+
 engine = create_engine(
     DATABASE_URL,
-    echo=True,
+    echo=False,
     pool_pre_ping=True,
 )
